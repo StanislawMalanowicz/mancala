082ccd5531cb21fe7170c783f16ef5f96981cd08
Staszek <beben86@wp.pl>
2020-09-16 22:10:23 +0200

    WiP

--- js/index.js
+++ js/index.js
@@ -7,13 +7,13 @@ const activeBoxes = document.querySelectorAll('.box');
 
 // console.log(activeBoxes)
 let currentBoxPoints;
-const lastIndex = 13;
+// const lastIndex = 13;
 
 const click = (element, index) => {
-  console.log('click', element," and ", index);
+  // console.log('click', element," and ", index);
   currentBoxPoints = parseInt(element.innerHTML);
   element.innerHTML = 0;
-  console.log('points: ',currentBoxPoints);
+  // console.log('points: ',currentBoxPoints);
 }
 
 allBoxes.forEach((box, index) =>{
@@ -22,68 +22,57 @@ allBoxes.forEach((box, index) =>{
     box.innerHTML = 4,
     box.addEventListener('click', () => {
       click(box, index);
-      console.log('current points: ', currentBoxPoints);
-      for(let i = index; i < currentBoxPoints+index; i++){
+      // console.log('current points: ', currentBoxPoints);
+      let lastCupIndex;
+      for(let i=index; i<currentBoxPoints+index; i++){
           // i>13 ? i = 0: null;
-          console.log('index nr: ', i);
-          const cup =  parseInt(allBoxes[i+1].innerHTML );
-          allBoxes[i+1].innerHTML = cup +1;
-          
-        };  
+          const cupNumber = i<13 ? i : i-14;
+          // console.log('index nr: ', cupNumber);
+          const cupPoints =  parseInt(allBoxes[cupNumber+1].innerHTML );
+          allBoxes[cupNumber+1].innerHTML = cupPoints +1;
+          lastCupIndex = i===currentBoxPoints+index-1 ? cupNumber < 6 ? cupNumber+1 : cupNumber-6  : null;
+          // console.log("last Cup index: ", lastCupIndex);
+        };
+        getOpositePoints(lastCupIndex, box, allBoxes);
+      // playerOne.checkScore();
+      // playerTwo.checkScore();
     })
-  ) : null
-    
-
-
+  ) : null  
 })
 
+const getOpositePoints = (lastIndex, selectedElement, eachElements) => {
+  if(lastIndex !== 6){
+    const enemyIndex = 5 - lastIndex;
+    console.log("lastIndex is: ",  lastIndex);
+    console.log("And oposite index is: ", enemyIndex);
+  };
+  console.log("is box class contains Two? ", selectedElement.parentNode.classList.contains("Two"));
+    
+};
+
 class Player{
-  constructor(scoreWrapper){
+  constructor(scoreWrapper, name){
+    this.name = name;
     this.points = 0;
     this.scoreWrapper = scoreWrapper;
     this.scoreWrapper.innerHTML = this.points;
+    this.gameOver = false;
     
+  }
+  checkScore(){
+    this.points = parseInt(this.scoreWrapper.innerHTML);
+    console.log(`player ${this.name} points: ${this.points}`);
   }
   
 }
 
-const playerOne = new Player(playerOnePoints);
+const playerOne = new Player(playerOnePoints, "PlayerOne");
 
-const playerTwo = new Player(playerTwoPoints);
-
-
+const playerTwo = new Player(playerTwoPoints, "PlayerTwo");
 
 
 
 
 
-// allBoxes.forEach((cup, index) => {
-//     1
-//     if (cup.className !== 'point') {
-//         let limit = 14;
-//         cup.addEventListener('click', function () {
-//             let cupPoints = Number(this.innerText);
-//             let count = index + 1;
-//             this.innerText = 0;
-//             for (let i = 0; i < cupPoints; i++) {
-//                 if (count < limit) {
-//                     var otherCupPoints = Number(allBoxes[count].innerText);
-//                     allBoxes[count].innerText = otherCupPoints + 1;
-//                     count += 1;
-//                 } else {
-//                     count = 0;
-//                     var otherCupPoints = Number(allBoxes[count].innerText);
-//                     allBoxes[count].innerText = otherCupPoints + 1;
-//                     count += 1;
-//                 }
 
-//             }
-//             let currentCup = allBoxes[count - 1].innerText;
-//             if (currentCup == 1 && allBoxes[count - 1].className !== 'point') {
-//                 allBoxes[count - 1].innerText = 0;
-//                 let plOnePts = Number(playerOnePoints.innerText);
-//                 playerOnePoints.innerText = plOnePts + 1;
-//             }
-//         })
-//     }
-// })
+
